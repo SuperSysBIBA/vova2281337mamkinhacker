849c9deded381fd9d32d35a0dcbea7b488fa432e
SuperSysBIBA <vvkvovavvk@gmail.com>
2025-03-29 17:22:53 +0700

    Add files via upload

--- NPCLOX.cs
+++ NPCLOX.cs
@@ -7,15 +7,18 @@ public class NPCLOX : MonoBehaviour
     public int health = 5;
     public int level = 1;
     public float speed = 1.2f;
+
+    public Vector3 newPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health += level;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       newPosition.x += speed * Time.deltaTime;
+       transform.position = newPosition;
     }
 }
