e2a1ae0fe5b74e91324dffde0f17f06bb5276516
SuperSysBIBA <vvkvovavvk@gmail.com>
2025-03-29 17:00:48 +0700

    Add files via upload

---
+++ NPCLOX.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCLOX : MonoBehaviour
+{
+    public int health = 5;
+    public int level = 1;
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
